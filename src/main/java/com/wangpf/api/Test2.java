c2284802a0e215ea08722f41d8303feaf1be0bec
guopengen <wpfysu@163.com>
2019-12-12 20:35:52 +0800

    主干分支测试

---
+++ src/main/java/com/wangpf/api/Test2.java
@@ -0,0 +1,10 @@
+package com.wangpf.api;
+
+/**
+ * Created by wangpengfei101022 on 2019/12/12.
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("我是主干分支，1.0分支你看不到我");
+    }
+}
